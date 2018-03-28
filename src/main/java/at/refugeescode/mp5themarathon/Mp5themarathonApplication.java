de9267784df00a15f3b85d455f4ed638ca5c0365
Hadi <hadi.effatian@gmail.com>
2018-03-28 15:29:54 +0200

    Recreated mp5

---
+++ src/main/java/at/refugeescode/mp5themarathon/Mp5themarathonApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp5themarathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp5themarathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp5themarathonApplication.class, args);
+	}
+}
